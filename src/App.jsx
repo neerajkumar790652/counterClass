fb83b290aa990c1003bf48edf4ba7c3b3180bc4c
neerajkumar790652 <92433406+neerajkumar790652@users.noreply.github.com>
2023-04-27 01:44:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    };
+  }
+
+  incrementCount = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  };
+
+  decrementCount = () => {
+    this.setState(prevState => ({
+      count: prevState.count - 1
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Current Count: {this.state.count}</p>
+        <button onClick={this.incrementCount}>Increment</button>
+        <button onClick={this.decrementCount}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
